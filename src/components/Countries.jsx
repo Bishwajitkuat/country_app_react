d57171f71eb2c51dce43db98e1f33c6f54d91ca5
Bishwajit Das <bishwajitkuat@gmail.com>
2023-09-21 21:06:37 +0300

    CountryCard componedt is created

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,25 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import { LinkContainer } from 'react-router-bootstrap';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import CountryCard from "./CountryCard";
+import { initializedCountries } from "../features/countries/countriesSlice";
 
 const Countries = () => {
-  const [search, setSearch] = useState('')
+  const dispatch = useDispatch();
+  const countriesList = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.isLoading);
 
-  console.log("Search: ", search)
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    dispatch(initializedCountries());
+  }, [dispatch]);
+
+  console.log("countriesList: ", countriesList);
 
   // We will be replacing this with data from our API.
-  const country = {
-    name: {
-      common: 'Example Country'
-    }
-  }
+  // const country = {
+  //   name: {
+  //     common: "Example Country",
+  //   },
+  // };
 
   return (
     <Container fluid>
@@ -27,7 +36,7 @@ const Countries = () => {
         <Col className="mt-5 d-flex justify-content-center">
           <Form>
             <Form.Control
-              style={{ width: '18rem' }}
+              style={{ width: "18rem" }}
               type="search"
               className="me-2 "
               placeholder="Search for countries"
@@ -38,36 +47,9 @@ const Countries = () => {
         </Col>
       </Row>
       <Row xs={2} md={3} lg={4} className=" g-3">
-            <Col className="mt-5">
-              <LinkContainer
-                to={`/countries/${country.name.common}`}
-                state={{ country: country }}
-              >
-                <Card className="h-100">
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>{'Single Country Common Name'}</Card.Title>
-                    <Card.Subtitle className="mb-5 text-muted">
-                      {'Single Country Official Name'}
-                    </Card.Subtitle>
-                    <ListGroup
-                      variant="flush"
-                      className="flex-grow-1 justify-content-end"
-                    >
-                      <ListGroup.Item>
-                        <i className="bi bi-translate me-2"></i>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <i className="bi bi-cash-coin me-2"></i>
-                      </ListGroup.Item>
-
-                      <ListGroup.Item>
-                        <i className="bi bi-people me-2"></i>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </Card.Body>
-                </Card>
-              </LinkContainer>
-            </Col>
+        {countriesList.map((item, index) => (
+          <CountryCard key={item.id + item.name.common} country={item} />
+        ))}
       </Row>
     </Container>
   );
--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import axios from "axios";
 
 const CountriesSingle = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [weather, setWather] = useState("");
+  const country = location.state.country;
+  const [errors, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+      )
+      .catch((err) => {
+        setError(true);
+      })
+      .then((res) => {
+        setWather(res.data);
+        setLoading(false);
+      });
+  }, [country.capital]);
+  console.log("weather :", weather);
 
   return (
     <Container>
