b16ab2efc019dcedebd3f2559ee4d589b2bef43a
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-10-19 17:10:57 +0300

    WeatherCard component layout is changed and css is being improved

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,21 +1,36 @@
 function WeatherCard({ weather, loading, errors, capital }) {
+  console.log(weather);
   return (
-    <div>
-      {errors && (
-        <p>Sorry, we don't have weather information for this country</p>
-      )}
-      {!errors && weather && (
-        <div>
+    <div className="container">
+      <div className="row justify-content-start">
+        <div className="col col-md-4">
           <p>
-            Right now it is <strong>{parseInt(weather.main.temp)}</strong>{" "}
-            degrees in {capital} and {weather.weather[0].description}
+            Temperature : <strong>{parseInt(weather.main.temp)} °C</strong>
           </p>
+          <p>
+            Feels Like : <strong>{parseInt(weather.main.feels_like)} °C</strong>
+          </p>
+          <p>
+            Humidity : <strong>{parseInt(weather.main.humidity)} %</strong>
+          </p>
+        </div>
+        <div className="col col-md-4">
           <img
+            className="img-fluid "
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
             alt={`${weather.weather[0].description}`}
           />
         </div>
+      </div>
+      <div className="row justify-content-start">
+        <p>
+          The weather in {capital} is {weather.weather[0].description}
+        </p>
+      </div>
+      {errors && (
+        <p>Sorry, we don't have weather information for this country</p>
       )}
+      {!errors && weather && <div></div>}
     </div>
   );
 }
